0f093c06ee3ed450e9f07b51831c04fb63d3e822
Luke McKinstry <lukemckinstry@gmail.com>
2020-02-27 12:02:21 -0500

    Updates

---
+++ precache-manifest.b406dc1e0f2f207333cd747f2f18a620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c94ecbb7a76f0021b57a367c5bafd0",
+    "url": "/ca-local-districts-storymap/index.html"
+  },
+  {
+    "revision": "a2afc1ee831de9f1897d",
+    "url": "/ca-local-districts-storymap/static/css/main.de63c54f.chunk.css"
+  },
+  {
+    "revision": "457d57303c61647c3191",
+    "url": "/ca-local-districts-storymap/static/js/2.3172dd64.chunk.js"
+  },
+  {
+    "revision": "a2afc1ee831de9f1897d",
+    "url": "/ca-local-districts-storymap/static/js/main.6e1de837.chunk.js"
+  },
+  {
+    "revision": "d20cd355f89b10220da7",
+    "url": "/ca-local-districts-storymap/static/js/runtime~main.851f1ed7.js"
+  }
+]);
